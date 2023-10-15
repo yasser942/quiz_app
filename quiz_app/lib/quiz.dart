b7a759867c81a988d6b1fe2fbfad32cf2e8faeff
kingn <yasseralhassan942@gmail.com>
2023-10-15 15:54:38 +0300

    last commit

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -2,51 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
   @override
-  State<Quiz> createState() => _QuizState();
+  State<Quiz> createState() {
+    return _QuizState();
+  }
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
-  var activeScreen = "start-screen";
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = "questions-screen";
+      activeScreen = 'questions-screen';
     });
   }
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        activeScreen = "start-screen";
-        selectedAnswers.clear();
+        activeScreen = 'results-screen';
       });
     }
   }
 
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 81, 4, 95),
-                Color.fromARGB(255, 119, 14, 137)
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
-            )),
-            child: activeScreen == "start-screen"
-                ? StartScreen(switchScreen)
-                : QuestionsScreen(onSelectAnswer: chooseAnswer)),
+            ),
+          ),
+          child: screenWidget,
+        ),
       ),
     );
   }
